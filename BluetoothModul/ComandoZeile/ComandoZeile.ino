c7df4056df0455f123a6d4926849b5a35a0d72ca
kloontim <timon.gronotte@hotmail.de>
2017-10-25 13:23:45 +0200

    changed to real Serial instead of Softwere

--- BluetoothModul/ComandoZeile/ComandoZeile.ino
+++ BluetoothModul/ComandoZeile/ComandoZeile.ino
@@ -10,32 +10,24 @@
  - SEE the comments after "//" on each line below
  - CONNECTIONS:
    - GND
-   - Pin z,B, 10 to HC-05 TXD
-   - Pin z.B. 11 to HC-05 RXD
+   - Pin      19 to HC-05 TXD
+   - Pin      18 to HC-05 RXD
    - Pin z.B. 2 to HC-05 EN
    - Pin z.B. VCC to HC-05 VCC for power control
    - Pin z.B. 3 to HC-05 State
  - V1.02 05/02/2015
    Questions: terry@yourduino.com */
 
-/*-----( Import needed libraries )-----*/
-#include <SoftwareSerial.h>  
 /*-----( Declare Constants and Pin Numbers )-----*/
-#define HC_05_TXD_ARDUINO_RXD 10
-#define HC_05_RXD_ARDUINO_TXD 11
 #define HC_05_EN        2
 #define HC_05_State     0 //has to be analog because voltage to small
 
-/*-----( Declare objects )-----*/
-SoftwareSerial BTSerial(HC_05_TXD_ARDUINO_RXD, HC_05_RXD_ARDUINO_TXD); // RX | TX
 /*-----( Declare Variables )-----*/
 //NONE
 
 void setup()   /****** SETUP: RUNS ONCE ******/
 {
-  pinMode(HC_05_EN, OUTPUT); 
-  pinMode(HC_05_State, INPUT);
-  
+  pinMode(HC_05_EN, OUTPUT);   
   digitalWrite(HC_05_EN, HIGH);  // Set command mode when powering up, if you want to use this as a serial playgound BETWEEN the two devices it needs to b3e low
   
   Serial.begin(9600);   // For the Arduino IDE Serial Monitor
@@ -43,7 +35,7 @@ void setup()   /****** SETUP: RUNS ONCE ******/
   Serial.println("LED should blink SLOWLY: 2 Seconds ON/OFF");
   delay(2000);
   Serial.println("Enter AT commands in top window.");
-  BTSerial.begin(38400);  // HC-05 default speed in AT command mode
+  Serial1.begin(38400);// HC-05 default speed in AT command mode
 
 }//--(end setup )---
 
@@ -51,16 +43,16 @@ void setup()   /****** SETUP: RUNS ONCE ******/
 void loop()   /****** LOOP: RUNS CONSTANTLY ******/
 {
   // READ from HC-05 and WRITE to Arduino Serial Monitor
-  if (BTSerial.available())
+  if (Serial1.available())
   {
-    Serial.write(BTSerial.read());
+    Serial.write(Serial1.read());
     //Serial.println("sollte lesen");
   }
 
   // READ Arduino Serial Monitor and WRITE to HC-05
   if (Serial.available())
   {
-    BTSerial.write(Serial.read());
+    Serial1.write(Serial.read());
     //Serial.println("soltel schreiben");
   }
 
