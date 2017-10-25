086ad2e3794014f18324f42c54b145fba00ff942
kloontim <timon.gronotte@hotmail.de>
2017-10-25 12:37:52 +0200

    Extendet Bluetooth Initialization

--- BluetoothModul/ComandoZeile/ComandoZeile.ino
+++ BluetoothModul/ComandoZeile/ComandoZeile.ino
@@ -12,8 +12,9 @@
    - GND
    - Pin z,B, 10 to HC-05 TXD
    - Pin z.B. 11 to HC-05 RXD
-   - Pin z.B.6 to HC-05 EN
-   - Pin z.B. 5+6 to HC-05 VCC for power control
+   - Pin z.B. 2 to HC-05 EN
+   - Pin z.B. VCC to HC-05 VCC for power control
+   - Pin z.B. 3 to HC-05 State
  - V1.02 05/02/2015
    Questions: terry@yourduino.com */
 
@@ -22,9 +23,8 @@
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define HC_05_TXD_ARDUINO_RXD 10
 #define HC_05_RXD_ARDUINO_TXD 11
-#define HC_05_SETUPKEY        4 //Following 3 lines stay unused
-#define HC_05_PWR1            5  // Connect in parallel to HC-05 VCC
-#define HC_05_PWR2            6  // Connect in parallel to HC-05 VCC
+#define HC_05_EN        4
+#define HC_05_State     5
 
 /*-----( Declare objects )-----*/
 SoftwareSerial BTSerial(HC_05_TXD_ARDUINO_RXD, HC_05_RXD_ARDUINO_TXD); // RX | TX
@@ -33,20 +33,14 @@ SoftwareSerial BTSerial(HC_05_TXD_ARDUINO_RXD, HC_05_RXD_ARDUINO_TXD); // RX | T
 
 void setup()   /****** SETUP: RUNS ONCE ******/
 {
-  pinMode(HC_05_SETUPKEY, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
-  pinMode(HC_05_PWR1, OUTPUT);      // Connect in parallel to HC-05 VCC
-  pinMode(HC_05_PWR2, OUTPUT);      // Connect in parallel to HC-05 VCC
+  pinMode(HC_05_EN, OUTPUT); 
+  pinMode(HC_05_State, INPUT);
   
-  digitalWrite(HC_05_SETUPKEY, HIGH);  // Set command mode when powering up
+  digitalWrite(HC_05_SETUPKEY, LOW);  // Set command mode when powering up
   
   Serial.begin(9600);   // For the Arduino IDE Serial Monitor
-  Serial.println("YourDuino.com HC-05 Bluetooth Module AT Command Utility V1.02");
   Serial.println("Set Serial Monitor to 'Both NL & CR' and '9600 Baud' at bottom right");
-  Serial.println("Vcc Power Up DELAY");
-  delay(2000);
   Serial.println("Applying VCC Power. LED should blink SLOWLY: 2 Seconds ON/OFF");
-  digitalWrite(HC_05_PWR1, HIGH); // Power VCC
-  digitalWrite(HC_05_PWR2, HIGH);  
   delay(2000);
   Serial.println("Enter AT commands in top window.");
   BTSerial.begin(38400);  // HC-05 default speed in AT command mode
