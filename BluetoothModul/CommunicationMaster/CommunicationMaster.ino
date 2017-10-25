6f4c9db2c1ead6c5b3fa71a1a83167e2d2e817f2
kloontim <timon.gronotte@hotmail.de>
2017-10-25 20:15:32 +0200

    ready for testing of connection setup

---
+++ BluetoothModul/CommunicationMaster/CommunicationMaster.ino
@@ -0,0 +1,58 @@
+#define STATE_PIN 0
+#define EN_PIN 2
+#define ERROR_PIN 13
+
+//varaibles
+char TestConnection = 't';
+int AnalogTreshhold = 500;
+int counter = 0;
+int SerialSpeed = 38400;
+int ModuloWait = 100000;
+
+void setup() {
+  // setting up the Pins to standart connection
+  pinMode(EN_PIN,OUTPUT);
+  pinMode(ERROR_PIN,OUTPUT);
+
+  digitalWrite(EN_PIN,LOW);
+  digitalWrite(ERROR_PIN,LOW);
+
+  //Start Serialport 0 for Error Messages
+  Serial.begin(9600);
+  Serial.println("Waiting for connection");
+  //wait until a connection has beend established
+  while(analogRead(STATE_PIN)<AnalogTreshhold)
+  {
+    if(counter%ModuloWait==0)//every ModuloWait times it sends a mmesage that it still struggles to connect
+    {
+      Serial.println("Still struggles with connection");
+    }
+    counter++;
+  }
+  counter = 0;
+  //Start and test the connection
+  Serial.println("Test the connection");
+  Serial1.begin(SerialSpeed);
+  Serial1.write(TestConnection);
+  while (!Serial1.available())
+  {
+    if(counter%ModuloWait==0)//every ModuloWait times it sends a mmesage that it still struggles to connect
+    {
+      Serial.println("Still waiting for answer struggles with connection ");
+      Serial.println("Resending teset");
+      Serial1.write(TestConnection);
+    }
+    counter++;
+  }
+  if(Serial1.read()!=TestConnection)
+  {
+    digitalWrite(ERROR_PIN,HIGH);
+    Serial.println("Test failed");
+  }
+  Serial.println("Starting loop");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
