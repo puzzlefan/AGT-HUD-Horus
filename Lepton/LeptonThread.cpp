ec5d8d0ca02606b5c3177a941dae9c99b5e1f7e2
kloontim <timon.gronotte@hotmail.de>
2017-10-26 16:17:35 +0200

    com on

--- Lepton/LeptonThread.cpp
+++ Lepton/LeptonThread.cpp
@@ -106,7 +106,7 @@ void LeptonThread::run() {
 				else
 					++sequence.back().second;
 #endif
-				if (packetNumber == 19) // %
+				if (packetNumber == 20) // %
 				{
           iSegment=packet[0];
           std::cout << "Segment Nr unverschoben: " << iSegment << std::endl;
